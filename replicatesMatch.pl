bd07a7a76a3bb64f145882344df629195c6c1576
Mikhail Matz <matz@utexas.edu>
2015-04-15 00:42:53 -0500

    added vcf2dadi.pl, new version of replicatesMatch.pl

--- replicatesMatch.pl
+++ replicatesMatch.pl
@@ -2,12 +2,12 @@
 
 my $usage="
 
-replicatesMatch.pl : (version 0.2, September 17, 2014)
+replicatesMatch.pl : (version 0.3, April 14, 2015)
 
-Selects variants that have identical genotypes among replicates.
+Selects polymorphic variants that have identical genotypes among replicates.  
 
-A genotype should be identical or missing; the fraction of allowed
-missing genotypes is controlled by missing=  argument.
+Genotypes of two replicates should be identical or missing; the maximal fraction of 
+genotype pairs with missing genotypes is controlled by missing=  argument.
 
 Arguments:
 
@@ -21,16 +21,11 @@ matching=[float] required fraction of matching genotypes
 
 missing=[float]  allowed fraction of missing genotypes, default 0.25
 
-altonly=[1|0]    only output SNPs showing alternative alleles (not necessarily
+allAlts=[1|0]    output all SNPs showing non-reference alleles (not necessarily
                  polymorphic among genotyped samples). Default 0
 
-polyonly=[1|0]   output only those passing SNPs that are polymorphic among
-                 replicated samples; for variant quality score recalibration 
-                 (VQSR) in GATK. Overrides altonly setting. Default 0
-
-min.falt=[float] minimum minor allele frequency. Default 1/(number of replicate pairs)
-
-max.falt=[float] maximum minor allele frequency. Default 0.35
+max.het=[float]  maximum fraction of heterozygotes (guards against lumped paralogous 
+                 loci). Default 0.75.
 
 Example: 
 replicatesMatch.pl vcf=cdh_alltags.ul_Variants_count10_ab10_sb10_clip0.vcf \
@@ -43,9 +38,7 @@ my $reps;
 my $missing=0.25;
 my $fmatch=1;
 my $altonly=0;
-my $polyonly=0;
-my $minfalt=0;
-my $maxfalt=0.35;
+my $maxhet=0.75;
 
 if ("@ARGV"=~/vcf=(\S+)/) { $vcf=$1;}
 else { die $usage; }
@@ -53,11 +46,8 @@ if ("@ARGV"=~/replicates=(\S+)/) { $reps=$1;}
 else { die $usage; }
 if ("@ARGV"=~/missing=(\S+)/) { $missing=$1;}
 if ("@ARGV"=~/matching=(\S+)/) { $fmatch=$1;}
-if ("@ARGV"=~/altonly=1/) { $altonly=1;}
-if ("@ARGV"=~/polyonly=1/) { $polyonly=1;}
-if ("@ARGV"=~/min.falt=(\S+)/) { $minfalt=$1;}
-if ("@ARGV"=~/max.falt=(\S+)/) { $maxfalt=$1;}
-
+if ("@ARGV"=~/allAlts=1/ ) { $altonly=1;}
+if ("@ARGV"=~/max.het=(\S+)/) { $maxhet=$1;}
 
 open VCF, $vcf or die "cannot open vcf file $vcf\n";
 
@@ -73,6 +63,7 @@ my $total=0;
 my $poly=0;
 my $numalt=0;
 my $numout=0;
+my $numhets=0;
 
 while (<VCF>) {
 	if ($_=~/^#/) {
@@ -106,6 +97,7 @@ while (<VCF>) {
 			close REP;
 			$minfalt=1/$nreps unless $nreps==0;
 #warn "$nreps replicate pairs\n";
+#warn join("\t",@npairs),"\n",join("\t",@pairs),"\n";
 		}
 		else { print $_;}
 		next;
@@ -126,12 +118,12 @@ while (<VCF>) {
 	my $a2;
 	my $nalt=0;
 	my $nref=0;
+	my $het=0;
 	for(my $p=0;$pp=$pairs[$p];$p++) {
 		($r1,$r2)=split(":",$npairs[$p]);
 		($i1,$i2)=split(":",$pp);
 		($g1,@rest)=split(":",$lin[$i1]);
 		($g2,@rest)=split(":",$lin[$i2]);
-#warn "$r1:$i1\t$g1\t$r2:$i2\t$g2\n";
 		if ($g1=~/\./ || $g2=~/\./) { 
 			$miss++;
 			$match++;
@@ -139,44 +131,36 @@ while (<VCF>) {
 		elsif ($g1 eq $g2) { 
 			$match++;
 			($a1,$a2)=split(/\D/,$g1);
-			if ($a1=~/[12345]/) { $nalt++;}
+			if ($a1=~/[123456789]/) { $nalt++;}
 			else { $nref++;}
-			if ($a2=~/[12345]/) { $nalt++;}			
+			if ($a2=~/[123456789]/) { $nalt++;}			
 			else { $nref++;}
-#warn "\t\t$a1 $a2\n";
-			if (!$seen{$a1}){
-				$seen{$a1}=1;
-				$anum++;
-			}
-			if (!$seen{$a2}){ 
-				$seen{$a2}=1;
-				$anum++;
-			}
+			if ($a1 ne $a2) {$het++;}
 		}
 	}
-#warn "\nmatch:$match\nmiss:$miss\nnref:$nref\nnalt:$nalt\nanum:$anum\n\nmatchtest:",$nreps*$fmatch,"\nmisstest:",sprintf("%.2f",$miss/$nreps),"\n\n";
 	next if ($match < ($nreps*$fmatch) );
 	next if ( ($miss/$nreps) > $missing);
 	if ($nalt) { $numalt++;}
-	if ($anum>1) { $poly++;}
-	if (!$altonly && !$polyonly) { 
-		print join("\t",@start)."\t".join("\t",@lin)."\n";
-	}
-	if ($nalt>$nref) { 
-		my $nn=$nalt;
-		$nalt=$nref;
-		$nfer=$nn;
-	}
-	elsif ($polyonly) {
-#warn "poly:\nfalttest:",sprintf("%.2f",$nalt/($nalt+$nref)),"\nfalt:$falt\n\n";
-		if ($anum>1 and $nalt/($nalt+$nref)>=$minfalt and $nalt/($nalt+$nref)<=$maxfalt) { 	
+	if ($nref and $nalt) { 
+		$poly++;
+		if ($het/$nreps<$maxhet) { 	
 			$numout++;
 			print join("\t",@start)."\t".join("\t",@lin)."\n";
-#warn join("\t",@samples)
+
+#warn "--------------\n$start[0]_$start[1]\n";
+#for(my $p=0;$pp=$pairs[$p];$p++) {
+#	($r1,$r2)=split(":",$npairs[$p]);
+#	($i1,$i2)=split(":",$pp);
+#	($g1,@rest)=split(":",$lin[$i1]);
+#	($g2,@rest)=split(":",$lin[$i2]);
+#warn "$r1:$i1\t$g1\t$r2:$i2\t$g2\n";
+#}
+#warn "het:$het nalt:$nalt nref:$nref miss:$miss match:$match\n";
+
 		}
 	}
 	elsif ($altonly){
-		if ($nalt) {
+		if ($nalt and $het/$nreps<$maxhet) {
 			$numout++;
 			print join("\t",@start)."\t".join("\t",@lin)."\n";
 		}
---
+++ vcf2dadi.pl
@@ -0,0 +1,123 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+use Bio::SeqIO;
+
+my $usage="
+
+Usage: perl vcf2dadi.pl <genome file> <vcf file> <list file>
+
+Genome file is in fasta format;
+List file gives population designations, like this:
+
+BEGIN
+sample1    population1
+sample2    population1
+sample3    population2
+END
+
+";
+
+if (!$ARGV[2]) { die $usage;}
+my ($file,$vcf,$list)=@ARGV;
+
+my %list;
+open(IN,"< $list")||die"$!";
+while (<IN>) {
+    chomp;
+    next if(/^\#/);
+    my @a=split(/\s+/);
+    $list{$a[0]}=$a[1];
+}
+close IN;
+
+my %vcf;
+my %pop;
+my %record;
+open(IN,"< $vcf");
+while (<IN>) {
+    chomp;
+    next if(/^##/);
+    if(/^#/){
+        my @a=split(/\s+/);
+        for(my $i=9;$i<@a;$i++){
+            next if(!exists $list{$a[$i]});
+            #die"$a[$i] does not exists in $list\n" if(!exists $list{$a[$i]});
+            $record{$i}=$list{$a[$i]};
+        }
+        next;
+    }
+    my @a=split(/\s+/);
+    my ($chr,$pos,$ref,$alt)=($a[0],$a[1],$a[3],$a[4]);
+    next if($alt=~/,/);
+
+    $vcf{$chr}{$pos}{ref}=$ref;
+    $vcf{$chr}{$pos}{alt}=$alt;
+#   print "--------------\n$ref\t$alt\n";
+    foreach my $i(keys %record){
+        my $indv=$record{$i};
+        $pop{$indv}=1;
+#       print "$indv\t";
+        my $geno=$a[$i];
+#       print "$geno\t";
+        $geno=~/^(.)[\/\|](.):/;
+        my $tempa=$1;
+        my $tempb=$2;
+#       print "$tempa $tempb\n";
+        my ($a1,$a2)=(0,0);
+        if($tempa eq "." || $tempb eq "."){
+            $a1=0;
+            $a2=0;
+        }
+        elsif ($tempa+$tempb == 1) {
+            $a1=1;
+            $a2=1;
+        }
+        elsif ($tempa+$tempb == 2) {
+            $a1=0;
+            $a2=2;
+        }
+        elsif ($tempa+$tempb == 0) {
+            $a1=2;
+            $a2=0;
+        }
+        $vcf{$chr}{$pos}{$indv}{a1}+=$a1;
+        $vcf{$chr}{$pos}{$indv}{a2}+=$a2;
+    }
+    #last;
+}
+close IN;
+
+open(O,"> $list.data");
+my $title="NAME\tOUT\tAllele1";
+foreach my $pop(sort keys %pop){
+    $title.="\t$pop";
+}
+$title.="\tAllele2";
+foreach my $pop(sort keys %pop){
+    $title.="\t$pop";
+}
+$title.="\tGene\tPostion\n";
+print O "$title";
+my $fa=Bio::SeqIO->new(-file=>$file,-format=>'fasta');
+while(my $seq=$fa->next_seq){
+    my $id=$seq->id;
+    my $seq=$seq->seq;
+    foreach my $pos (sort {$a<=>$b} keys %{$vcf{$id}}){
+        my $ref=substr($seq,$pos-2,3);
+        my $line="$ref\t$ref\t$vcf{$id}{$pos}{ref}";
+        foreach my $pop(sort keys %pop){
+            my $num=$vcf{$id}{$pos}{$pop}{a1};
+            $line.="\t$num";
+        }
+        $line.="\t$vcf{$id}{$pos}{alt}";
+        foreach my $pop(sort keys %pop){
+            my $num=$vcf{$id}{$pos}{$pop}{a2};
+            $line.="\t$num";
+        }
+        $line.="\t$id\t$pos\n";
+        print O "$line";
+    }
+}
+close O;
+
