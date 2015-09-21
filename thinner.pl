fdf23f5d182d0954dc398742503e32a8b0dbf5d6
Mikhail Matz <matz@utexas.edu>
2015-09-20 20:12:49 -0500

    thinner v4

--- thinner.pl
+++ thinner.pl
@@ -2,16 +2,21 @@
 
 my $usage="
 
-thinner.pl (v.3) : 
+thinner.pl (v.4) : 
 
 Leaves only one SNP per specified nucleotide distance interval, chosen by its 
 allele frequency, sequencing depth, combination of those, or randomly.
+
+The minimal distance between selected SNPs is half the specified interval.
+
+The thinning process uses random starts for interval specification, potentially
+resulting in different SNPs selected. 
 
 Arguments:
 
                vcf=[file name] : vcf file name
             
-            interval=[integer] : interval length, default 40 (for 2bRAD tags)
+            interval=[integer] : interval length, default 80 (for 2bRAD tags)
             
 criterion=[maxAF|maxDP-random|maxDP-maxAF|random] : SNP choosing criterion.
                              maxAF - maximum minor allele frequency;
@@ -27,13 +32,13 @@ Example: thinner.pl vcf=denovo.recal.vcf > thinDenov.vcf
 
 NOTES: - Do not thin variants if you want to calculate pi or Tajima's D.
 
-Mikhail Matz, matz@utexas.edu
+Mikhail Matz, matz\@utexas.edu
 
 ";
 
 my $vcf;
 if (" @ARGV "=~/vcf=(\S+)/) { $vcf=$1; } else { die $usage; }
-my $inter=40;
+my $inter=80;
 if (" @ARGV "=~/interval=(\d+)/) { $inter=$1; } 
 my $criterion="maxDP-maxAF";
 if (" @ARGV "=~/criterion=maxAF/) { $criterion="maxAF"; } 
@@ -48,7 +53,9 @@ my @dats;
 my $info;
 my $chrom;
 my $pos;
-my $pos0=-1000000;
+my $pos0=-sprintf("%.0f",rand $inter/2);
+my $poslast=-$inter;
+#warn "pos0:$pos0;poslast:$poslast\n";
 my $af;
 my $dp;
 my @afs=();
@@ -64,12 +71,18 @@ while (<VCF>) {
 	chomp;
 	@dats=split(/\t/,$_);
 	$chrom=$dats[0];
+	if (!$chrom0) { $chrom0=$chrom;}
 	$pos=$dats[1];
+	if ( $pos-$poslast < $inter/2 and ($chrom eq $chrom0)) {
+#warn "\t\t\tskipping $pos\n" ;
+		next;
+	}
 	$info=$dats[7];
 	if ($info=~/AF=(\S*?);/) { $af=$1; } else { warn "no AF:\n@dats\n" and next;}
 	if ($info=~/DP=(\d*?);/) { $dp=$1; } else { warn "no DP:\n@dats\n" and next;}
 	if ($af>0.5) { $af=1-$af; }
-	if ($pos-$pos0 <=$inter and ($chrom eq $chrom0)){
+	if (($pos-$pos0 < $inter) and ($chrom eq $chrom0)){
+#warn "\t pos:$pos - interval:",$pos0,"-",$pos0+$inter," - poslast:$poslast\n";
 		push @snps,join("\t",@dats);
 		push @afs,$af;
 		push @dps,$dp;
@@ -83,10 +96,17 @@ while (<VCF>) {
 	}
 	else {
 		if ($snps[0]){
-#warn "$pos0: $#snps | @afs | @dps :  maxaf:$afs[$maxaf]; maxdp:$dps[$maxdp] (@maxdps) \n";
-			if ($criterion eq "random") { print $snps[rand @snps],"\n" ; }
-			elsif ($criterion eq "maxAF") { print $snps[$maxaf] ,"\n"  ; }
-			elsif ($criterion eq "maxDP-random"){ print $snps[$maxdps[rand @maxdps]] ,"\n" ; }
+			my $datum;
+#warn "$chrom0: $#snps | @afs | @dps :  maxaf:$afs[$maxaf]; maxdp:$dps[$maxdp] (@maxdps) \n";
+			if ($criterion eq "random") { 
+				$datum=$snps[rand @snps];
+			}
+			elsif ($criterion eq "maxAF") { 
+				$datum=$snps[$maxaf] ;
+			}
+			elsif ($criterion eq "maxDP-random"){ 
+				$datum=$snps[$maxdps[rand @maxdps]] ; 
+			}
 			elsif ($criterion eq "maxDP-maxAF"){
 				$maxaf=0;
 				foreach my $md (@maxdps) { 
@@ -95,39 +115,64 @@ while (<VCF>) {
 						$maxdp=$md;
 					}
 				} 	
-				print $snps[$maxdp] ,"\n" ; 
+				$datum=$snps[$maxdp] ; 
 #warn "chose $maxdp\n";
 			}
+			my @datss=split(/\t/,$datum);
+			$poslast=$datss[1];
+#warn "\t\tSel:$poslast\n";
+			print $datum,"\n" ; 				 
 		}
-		$pos0=$pos;
-		$chrom0=$chrom;
 		$maxaf=0;
 		$maxdp=0;
 		@afs=();
 		@dps=();
 		@snps=();
+		if ( $pos-$poslast < $inter/2 and ($chrom eq $chrom0)) {
+#warn "\t\t\tskipping $pos\n" ;
+			next;
+		}
+		if ($chrom ne $chrom0) {
+			$chrom0=$chrom;
+#warn "\n-------------\nnew chrom: $chrom\n";
+			$poslast=-$inter;
+			$pos0=-sprintf("%.0f",rand $inter/2);
+		}	
+		while ($pos0+$inter<$pos) { $pos0+=$inter;}
+#warn "\t Pos:$pos - interval:",$pos0,"-",$pos0+$inter," - poslast:$poslast\n";
 		push @afs,$af;
 		push @dps,$dp;
 		@maxdps=();
 		push @maxdps, 0; 
-		push @snps,join("\t",@dats);
+		push @snps,join("\t",@dats) if $pos-$poslast > $inter/2;
 	}
 }
 
-if ($snps[0]){
-#warn "$pos0: $#snps | @afs | @dps :  maxaf:$afs[$maxaf]; maxdp:$dps[$maxdp] (@maxdps) \n";
-	if ($criterion eq "random") { print $snps[rand @snps],"\n" ; }
-	elsif ($criterion eq "maxAF") { print $snps[$maxaf] ,"\n"  ; }
-	elsif ($criterion eq "maxDP-random"){ print $snps[$maxdps[rand @maxdps]] ,"\n" ; }
-	elsif ($criterion eq "maxDP-maxAF"){
-		$maxaf=0;
-		foreach my $md (@maxdps) { 
-			if ($afs[$md]>$maxaf) {
-				$maxaf=$afs[$md];
-				$maxdp=$md;
+		if ($snps[0]){
+			my $datum;
+#warn "$chrom0: $#snps | @afs | @dps :  maxaf:$afs[$maxaf]; maxdp:$dps[$maxdp] (@maxdps) \n";
+			if ($criterion eq "random") { 
+				$datum=$snps[rand @snps];
 			}
-		} 	
-		print $snps[$maxdp] ,"\n" ; 
+			elsif ($criterion eq "maxAF") { 
+				$datum=$snps[$maxaf] ;
+			}
+			elsif ($criterion eq "maxDP-random"){ 
+				$datum=$snps[$maxdps[rand @maxdps]] ; 
+			}
+			elsif ($criterion eq "maxDP-maxAF"){
+				$maxaf=0;
+				foreach my $md (@maxdps) { 
+					if ($afs[$md]>$maxaf) {
+						$maxaf=$afs[$md];
+						$maxdp=$md;
+					}
+				} 	
+				$datum=$snps[$maxdp] ; 
 #warn "chose $maxdp\n";
-	}
-}
+			}
+			my @datss=split(/\t/,$datum);
+			$poslast=$datss[1];
+#warn "\t\tSel:$poslast\n";
+			print $datum,"\n" ; 				 
+		}
