5f6998c4a58895492ff4396d87630d4e43fd7137
Mikhail Matz <matz@utexas.edu>
2017-11-07 13:46:11 +0100

    dadi2sfs

---
+++ dadi2sfs_fold.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import moments
+import numpy as np
+from numpy import array
+from moments import Misc,Spectrum
+import sys
+
+infile=sys.argv[1]
+pop_ids=[sys.argv[2]]
+projections=[int(sys.argv[3])]
+outname='SFSfold_'+infile
+
+dd = Misc.make_data_dict(infile)
+data = Spectrum.from_data_dict(dd, pop_ids,projections,polarized=False)
+Spectrum.to_file(data,outname,precision=4,foldmaskinfo=False)
