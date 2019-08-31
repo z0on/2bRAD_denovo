127b7557feeb0859452c240d8994804a97fb5b24
Mikhail Matz <matz@utexas.edu>
2019-08-31 14:11:02 -0500

    big fix in ld2matrix

--- ld2matrix.R
+++ ld2matrix.R
@@ -1,24 +1,24 @@
 fin = commandArgs(T)
-
+fin="LD_1.LD.slim"
 message("reading input...")
-ald=read.table(fin,sep="\t") #[,c(1,2,7)]
+ald=read.table(fin,sep="\t",stringsAsFactors=FALSE) #[,c(1,2,7)]
 message("...done")
 names(ald)=c("s1","s2","rEM")
-sites=sort(unique(c(as.character(ald$s1),as.character(ald$s2))))
+sites=sort(unique(c(ald$s1,ald$s2)))
 nsites=length(sites)
 ldmat=matrix(-1,nrow=nsites,ncol=nsites)
 dimnames(ldmat)=list(sites,sites)
 message("analyzing sites...")
-for (i in 1:(nsites-1)) {
+for (i in 1:nsites) {
 	message(paste(i,":",sites[i]))
 	sub=subset(ald,s1==sites[i] | s2==sites[i])
+	nrow(sub)
 	swi=which(sub$s2==sites[i])
 	sub$s2[swi]=sub$s1[swi]
-#	print(sub[1:5,])
+	row.names(sub)=sub$s2
 	for (j in which(ldmat[,i]==-1)) {
 		if (i==j) { next }
-#		message("    ",j,sub$s2[j])
-	 	ldmat[sites[i],sub$s2[j]]=ldmat[sub$s2[j],sites[i]]=sub[j,"rEM"]
+	 	ldmat[sites[i],sites[j]]=ldmat[sites[j],sites[i]]=sub[sites[j],"rEM"]
 	}
 }
 diag(ldmat)=1
